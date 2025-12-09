25d3db2b3b38ac6b8f84a9f1631139052dcdaa5b
OscarvdLubbe <40429@ma-web.nl>
2025-12-09 13:08:01 +0100

    Caracter _ist

---
+++ M2ProgCs2_Opdrachten/Lists/Lists/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Lists;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.run();
+    }
+    internal void run()
+    {
+        string[] caracters = {
+        "Pac-Man",
+        "Samus",
+        "Crash Bandicoot",
+        "Spyro the Dragon",
+        "Donkey Kong",
+        "Mario",
+        "Luigi",
+        "Astro",
+        "Solid Snake",
+        "Master Chief",
+        "Kratos",
+        "Sonic the Hedgehog",
+        "Link",
+
+
+    };
+        List<string> charaterList = new List<string>();
+        
+            for (int i = 0; i < caracters.Length; i++)
+            {
+                charaterList.Add(caracters[i]);
+            }
+
+            for (int i = 0; i < charaterList.Count; i++)
+            {
+                Console.WriteLine(charaterList[i]);
+            }
+        
+    }
+
+}
