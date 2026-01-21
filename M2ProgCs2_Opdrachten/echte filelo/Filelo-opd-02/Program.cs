7211a93fed2cec549840d167ada78f528bd0c6e2
OscarvdLubbe <40429@ma-web.nl>
2026-01-21 12:09:33 +0100

    SaveFile

---
+++ M2ProgCs2_Opdrachten/echte filelo/Filelo-opd-02/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace echte_filelo;
+
+class Program
+{
+    static string welkomTekst = "Hello world";
+    static string saveFile = "welkomState.txt";
+    static void Main(string[] args)
+    {
+        Run();
+    }
+    static void Run()
+    {
+        while (true)
+        {
+            Console.WriteLine($"{welkomTekst}");
+            Console.WriteLine("Enter a text, then press enter");
+            welkomTekst = Console.ReadLine();
+            File.WriteAllText(saveFile, welkomTekst);
+            bool bestaatDeFile = File.Exists(saveFile);
+            if (bestaatDeFile == true)
+            {
+                Console.WriteLine(File.ReadAllText(saveFile));
+            }
+        }
+    }
+}
