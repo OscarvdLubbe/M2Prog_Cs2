4b946a06166da9f2efc7ead8a75da34531fe735c
OscarvdLubbe <40429@ma-web.nl>
2026-01-26 12:49:03 +0100

    jsonfile

---
+++ M2ProgCs2_Opdrachten/echte filelo/Filelo-opd-03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Filelo_opd_03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Run();
+    }
+    static void Run()
+    {
+        
+    }
+}
+public class Product
+{
+    public string Name {get; set;}
+    public string Description {get; set;}
+    public double Price {get; set;}
+}
+
